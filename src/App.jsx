fef14f2e3e395d591fd11fdf83f7415d4d15c3a2
breaper03 <breaper2021@gmail.com>
2023-07-03 21:38:42 -0400

    fix 4

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,6 @@
 import './App.css'
 import Navbar from './Components/Navbar/Navbar'
-import About from './Components/first_section_about/about'
+import About from './Components/first_section_about/About'
 import Experience from './Components/experience_section/Experience'
 import Projects from './Components/projects_section/Projects'
 import Skills from './Components/skills_section/Skills'
